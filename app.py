8623713c7050d80a67eb77fa81a3b215b2a288dc
houjianyu <william.jianyuhou@onespan.com>
2022-03-06 22:51:29 -0500

    Change to use Port 10000 in my HomeNAS

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
 
 # run the app.
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
